24e885cb023332529845a3e843f0316bbed5b2ee
Snowy <45012141+SnowyWhite@users.noreply.github.com>
2025-05-03 20:39:07 +0200

    fix: Make code runnable

--- bootstrap/bootstrap/bootstrap.cxx
+++ bootstrap/bootstrap/bootstrap.cxx
@@ -1,21 +1,18 @@
 #include <libbootstrap/bootstrap.hxx>
 #include <iostream>
-#include <windows.h>
 
 int __cdecl
 main (const int argc, char* argv[])
 {
   using namespace std;
 
-  if (argc < 2)
-  {
-    cerr << "error: missing dll name" << endl;
-    return 1;
-  }
-
   bootstrap::say_hello (cout, argv[1]);
 
-  cout << "Loading " << argv[1] << '!' << endl;
-
-  ::LoadLibraryA (argv[1]);
+  // if (argc < 2)
+  // {
+  //   cerr << "error: missing dll name" << endl;
+  //   return 1;
+  // }
+  // cout << "Loading " << argv[1] << '!' << endl;
+  // ::LoadLibraryA (argv[1]);
 }
